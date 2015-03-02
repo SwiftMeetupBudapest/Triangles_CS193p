7bce9cc1b281e6f63d675f37fee012c47f0fd328
Miklo Geza <geza.miklo@centralmediacsoport.hu>
2015-03-02 22:32:53 +0100

    Triangle example with pinch and rotate

---
+++ Triangles/TriangleView.swift
@@ -0,0 +1,66 @@
+//
+//  TriangleView.swift
+//  TrianglesExample
+//
+//  Created by Géza Mikló on 02/03/15.
+//  Copyright (c) 2015 Géza Mikló. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TriangleView: UIView {
+
+    //Has to be CGFloat, Double value won't show up in Inspector
+    @IBInspectable
+    var scale : CGFloat = 0.5 {
+            didSet {
+                setNeedsDisplay()
+            }
+        }
+    var rotation: CGFloat = 0 {
+        didSet {
+            self.transform = CGAffineTransformMakeRotation(CGFloat( rotation ) )
+            setNeedsDisplay()
+        }
+    }
+    
+    func scaleAction(gesture: UIPinchGestureRecognizer) {
+        scale = gesture.scale * scale
+        gesture.scale = 1
+    }
+
+    func rotateAction(gesture: UIRotationGestureRecognizer) {
+        switch gesture.state {
+        case .Changed:
+            rotation += gesture.rotation
+            gesture.rotation = 0
+        default:
+            break;
+        }
+    }
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        let path = UIBezierPath();
+
+        let w = (self.bounds.width ?? 320) * scale
+        let h = (self.bounds.height ?? 480) * scale
+        let hCenter = w / 2
+        let vFourth = h / 4
+
+        path.moveToPoint(CGPoint(x: CGFloat(hCenter), y: CGFloat(vFourth)))
+        path.addLineToPoint(CGPoint(x: CGFloat(w * 0.9), y: CGFloat(h*0.9)))
+        path.addLineToPoint(CGPoint(x: CGFloat(w*0.1), y: CGFloat(h*0.9)))
+        path.lineWidth = 3
+        
+        path.closePath()
+        UIColor.redColor().setFill()
+        UIColor.greenColor().setStroke()
+        
+        path.stroke()
+        path.fill()
+    }
+
+}
---
+++ Triangles/TrianglesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TrianglesExample
+//
+//  Created by Géza Mikló on 02/03/15.
+//  Copyright (c) 2015 Géza Mikló. All rights reserved.
+//
+
+import UIKit
+
+class TrianglesViewController: UIViewController {
+
+    @IBOutlet weak var triangleView: TriangleView! {
+        didSet {
+            triangleView.addGestureRecognizer(UIPinchGestureRecognizer(target: triangleView, action: "scaleAction:"))
+            triangleView.addGestureRecognizer(UIRotationGestureRecognizer(target: triangleView, action: "rotateAction:"))
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
